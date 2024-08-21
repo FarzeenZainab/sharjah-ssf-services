7f3d1bb9b1179dae4074ab030978e9df309a27ce
FarzeenZainab <farzeenzaidi51@gmail.com>
2024-08-22 01:55:17 +0500

    board of directors

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/landing/hero-section";
 import OurServices from "@/components/landing/our-services";
+import BoardOfDirectors from "@/components/landing/board-of-directors";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <OurServices />
+      <BoardOfDirectors />
     </>
   );
 }
---
+++ src/components/landing/board-of-directors.jsx
@@ -0,0 +1,21 @@
+import Section from "@/components/section";
+import Container from "@/components/container";
+
+const BoardOfDirectors = () => {
+  return (
+    <div className="bg-[url('/images/shapes.png')] bg-cover bg-no-repeat">
+      <Container>
+        <Section>
+          <div className="mb-[64px]">
+            <h3 className="text-center text-primary">Board Of Directors</h3>
+            <h2 className="text-primary font-medium text-center ">
+              Meet Our Esteemed Board of Directors
+            </h2>
+          </div>
+        </Section>
+      </Container>
+    </div>
+  );
+};
+
+export default BoardOfDirectors;
