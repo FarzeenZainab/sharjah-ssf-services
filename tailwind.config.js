13b9071132189b88c57c2f84dcf875e0427e1d1f
FarzeenZainab <farzeenzaidi51@gmail.com>
2024-08-21 22:08:40 +0500

    added font families

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        "pf-din": ["var(--font-pf-din)"],
+        abhaya: ["var(--font-abhaya)"],
+      },
     },
   },
   plugins: [],
