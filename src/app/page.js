896b260ceca4efcc17fb75469eca31e2725c5e8f
FarzeenZainab <farzeenzaidi51@gmail.com>
2024-08-21 22:22:15 +0500

    added resposive font sizes

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
 export default function Home() {
   return (
     <>
-      <h1>This is heading 1</h1>
-      <h2>This is heading 2</h2>
+      <h1>Protect your presence secure your future</h1>
+      <h2>Discover Our Services</h2>
+      <h3>Periodic Review</h3>
+      <p>
+        Security in the Emirate of Sharjah plays an important role in achieving
+        job stability and strengthening the employee&apos;s relationship...
+      </p>
     </>
   );
 }
